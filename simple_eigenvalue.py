ad91607383d3038597bde886c97aa26ec0151701
tjlaboss <travis@ktc-technology.com>
2017-12-10 23:37:18 -0500

    First attempt at Coarse Mesh Rebalance method

--- simple_eigenvalue.py
+++ simple_eigenvalue.py
@@ -199,7 +199,7 @@ Indices:
 		Edit: just expanded him to multigroup too
 		"""
 		ss = np.zeros((self.nx, self.groups))
-		'''
+		#'''
 		for i in range(self.nx):
 			phi_vector = self.flux[i]
 			ss[i] = self.nodes[i].scatter_matrix.dot(phi_vector)
@@ -214,6 +214,7 @@ Indices:
 						ss[i, g] += phi*node.scatter_matrix[g, gp]
 			else:
 				ss[i] = self.flux[i]*node.scatter_matrix
+		'''
 		return ss
 	
 	def _populate(self):
@@ -229,14 +230,134 @@ Indices:
 				self.nodes[i] = mod_node
 
 
-# test
+class RebalancePincell1D(Pincell1D):
+	"""A Coarse Mesh Rebalance formulation of the 1D pincell"""
+	def __init__(self, quad=None, mod=None, fuel=None,
+	             nx_mod=None, nx_fuel=None, groups=None, ratio=None):
+		if not ((quad is None) or (groups is None) or (ratio is None)):
+			super().__init__(quad, mod, fuel, nx_mod, nx_fuel, groups)
+			self.ratio = ratio
+			self.currents = np.zeros((2, self.nx, self.groups))
+			self._populate()
+		self.psi = None
+		
+	def fromFineMesh(self, fine_mesh, ratio):
+		"""Create a RebalanceMesh1D using the transport mesh as a template.
+		Or another coarse mesh, CMR is naturally multi-level.
+
+		TODO: Allow mixed fuel/moderator nodes.
+
+		Parameters:
+		-----------
+		fine_mesh:      Mesh1D to base the coarse mesh off
+		nx:             int; number of x-nodes in the coarse mesh
+		"""
+		assert not fine_mesh.nx % ratio, \
+			"Ratio does not produce an integer number of fuel nodes."
+		assert not fine_mesh.nx_mod % ratio, \
+			"Ratio does not produce an integer number of moderator nodes."
+		nx_fuel = fine_mesh.nx_fuel // ratio
+		nx_mod = fine_mesh.nx_mod // ratio
+		cm = RebalancePincell1D(fine_mesh.quad, fine_mesh.mod, fine_mesh.fuel,
+		                        nx_mod, nx_fuel, fine_mesh.groups, ratio)
+		return cm
+	
+	def _restrict_cross_sections(self):
+		"""Placeholder for cross section restriction
+		
+		This will integrate each macro_xs over the volume of each node
+		and basically imitate the Pincell1D._populate() function.
+		
+		This will be written once mixed fuel/mod cells are enabled.
+		"""
+		pass
+	
+	def _get_rebalance_factors(self, new_flux):
+		"""Find the rebalance factors for prolongation.
+		
+		Parameter:
+		----------
+		new_flux:   array of the scalar flux on the coarse mesh
+					after the latest linear solution
+		
+		Returns:
+		--------
+		factors:    array of rebalance factors on the coarse mesh
+		"""
+		return new_flux/self.flux
+	
+	def restrict_flux(self, fine_mesh):
+		"""Integrate the angular flux from the transport solution over
+		the coarse mesh cells. Use it to update	the coarse mesh scalar flux.
+		
+		Parameter:
+		----------
+		fine_mesh:      Pincell1D instance
+		"""
+		for g in range(self.groups):
+			for cmi in range(self.nx):
+				phi = 0.0
+				jplus = 0.0
+				jminus = 0.0
+				# Integrate over the fine mesh
+				for fmi in range(self.ratio):
+					i = self.ratio*cmi + fmi
+					dxi = fine_mesh.nodes[i].dx
+					for n in range(self.quad.N):
+						psi_n0 = fine_mesh.psi[i, n, g]#*dxi     # LHS integrated flux
+						psi_n1 = fine_mesh.psi[i + 1, n, g]#*dxi # RHS integrated flux
+						if n < self.quad.N2:
+							jplus += self.quad.weights[n]*abs(self.quad.mus[n])*psi_n1
+						else:
+							jminus += self.quad.weights[n]*abs(self.quad.mus[n])*psi_n0
+						phi += 0.5*(psi_n0 + psi_n1)  # diamond difference approxmation
+				self.currents[:, cmi, g] = jplus, jminus
+				self.flux[cmi, g] = phi
+	
+				
+	def prolong_flux(self, fine_mesh, coarse_flux):
+		"""Use the coarse mesh flux to update the fine mesh flux.
+		This RebalancePincell1D's flux will be updated to the
+		coarse flux after the prolongation is complete.
+		
+		Parameter:
+		----------
+		fine_mesh:      Pincell1D instance
+		coarse_flux:    array of the new coarse flux to use for the prolongation.
+		"""
+		factors = self._get_rebalance_factors(coarse_flux)
+		for i in range(fine_mesh.nx):
+			cmi = i // self.ratio
+			fi = factors[cmi]
+			for g in range(self.groups):
+				fine_mesh.flux[i, g] *= fi[g]
+				# TODO: Confirm whether the angular flux should be likewise updated
+				fine_mesh.psi[i, :, g] *= fi[g]
+		self.flux = coarse_flux
+		
+	
+
+# test fine mesh
 s2 = quadrature.GaussLegendreQuadrature(2)
 NXMOD = 0
 NXFUEL = 8
 KGUESS = kinf
 cell = Pincell1D(s2, mod_mat, fuel_mat, nx_mod=NXMOD, nx_fuel=NXFUEL, groups=G)
+
 solver = calculator.DiamondDifferenceCalculator1D(s2, cell, ("reflective", "reflective"), kguess=KGUESS)
-#solver.transport_sweep(KGUESS)
+solver.transport_sweep(KGUESS)
+
+# test CMR
+coarse_mesh = RebalancePincell1D().fromFineMesh(cell, 2)
+coarse_mesh.restrict_flux(cell)
+coarsine = np.array([np.cos((i-1.5)*np.pi/coarse_mesh.nx) for i in range(coarse_mesh.nx)])
+new_flux = np.empty((coarse_mesh.nx, G))
+new_flux[:, 0] = coarsine
+new_flux[:, 1] = 0.5*coarsine
+coarse_mesh.prolong_flux(cell, new_flux)
+
+raise SystemExit
+
 converged = solver.solve(eps=1E-6, maxiter=200)
 phi = solver.mesh.flux
 print(cell)
